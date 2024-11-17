b90519ac06ccd4ce1dd2fc2198c7d5534d01f4dd
mitnalakrishnaprasad <navarasakp@gmail.com>
2024-11-17 16:34:38 +0530

    this is my first project

---
+++ src/main/java/mysmartmedia/com/project_new/App.java
@@ -0,0 +1,10 @@
+package mysmartmedia.com.project_new;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
